fdada283854ff73c3267e8ddf12e66d6836dcbce
Diego Caruba <diego.caruba.carmo@ibm.com>
2023-02-10 14:56:21 -0300

    fix: correct server.js to heroku

--- server.js
+++ server.js
@@ -4,16 +4,27 @@ const app = express();
 const http = require('http').Server(app);
 const serverSocket = require('socket.io')(http);
 
-const PORT = 3000;
+const PORT = process.env.PORT || 8000;
 
 app.use(express.static('public'));
 
+const host = process.env.HEROKU_APP_NAME ? `https://${process.env.HEROKU_APP_NAME}.herokuapp.com` : "http://localhost"
+
+http.listen(PORT, () => {
+  const portaStr = (PORT === 80) ? '' : `:${PORT}`
+  
+  if (process.env.HEROKU_APP_NAME)
+    console.log(`Server running on ${host}`);
+  else 
+    console.log(`Server running on ${host+portaStr}`);
+    
+});
+
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
-})
+});
 
 serverSocket.on('connection', (socket) => {
-  // console.log('Socket Handshake: ', socket.handshake);
   
   socket.on('login', (nickname) => {
     console.log('Client is connected: ', nickname);
@@ -28,7 +39,3 @@ serverSocket.on('connection', (socket) => {
     serverSocket.emit('chat msg', `${socket.nickname}: ${msg}`);
   });
 });
-
-http.listen(PORT, () => {
-  console.log(`Server listening on http://localhost:${PORT}`);
-})
